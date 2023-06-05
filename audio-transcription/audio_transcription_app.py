4ee8eff4e0f48ddc7230fe9267fbe44e1c58dcec
Eduardo <110411487+Esalvador1998@users.noreply.github.com>
2023-06-04 20:17:53 -0400

    Add files via upload

---
+++ audio-transcription/audio_transcription_app.py
@@ -0,0 +1,29 @@
+import gradio as gr
+import whisper
+import certifi
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+ssl._create_default_https_context(cafile=certifi.where())
+
+
+def transcribe_audio(audio_file):
+    model = whisper.load_model("base")
+    result = model.transcribe(audio_file)
+    return result["text"]
+
+
+def main():
+    audio_input = gr.inputs.Audio(source="upload", type="filepath")
+    output_text = gr.outputs.Textbox()
+    
+    iface = gr.Interface(fn=transcribe_audio, inputs=audio_input, 
+                         outputs=output_text, title="Eduardo's Audio Transcription App",
+                         description="Upload an audio file and hit the 'Submit'\
+                             button")
+    
+    iface.launch()
+
+
+if __name__ == '__main__':
+    main()
